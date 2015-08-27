66720c6c0797248c69d264e16094ed9418a43b9a
elviswf <wf616nb@163.com>
2015-08-27 16:53:19 +0800

    On branch master
     Changes to be committed:
    	modified:   .meteor/packages
    	modified:   .meteor/versions
    	new file:   client/helpers/config.js
    	modified:   client/main.html
    	modified:   client/main.js
    	new file:   client/templates/application/layout.html
    	new file:   client/templates/application/layout.js
    	new file:   client/templates/application/not_found.html
    	new file:   client/templates/comments/comment_item.html
    	new file:   client/templates/comments/comment_item.js
    	new file:   client/templates/comments/comment_submmit.html
    	new file:   client/templates/comments/comment_submmit.js
    	new file:   client/templates/include/access_denied.html
    	new file:   client/templates/include/header.html
    	new file:   client/templates/include/loading.html
    	new file:   client/templates/notifications/notifications.html
    	new file:   client/templates/notifications/notifications.js
    	modified:   client/templates/posts/post_item.js
    	new file:   client/templates/posts/post_submit.js
    	new file:   client/templates/posts/posts_edit.html
    	new file:   client/templates/posts/posts_edit.js
    	modified:   client/templates/posts/posts_item.html
    	modified:   client/templates/posts/posts_list.js
    	new file:   client/templates/posts/posts_page.html
    	new file:   client/templates/posts/posts_page.js
    	new file:   client/templates/posts/posts_submit.html
    	new file:   lib/connections/comments.js
    	modified:   lib/connections/posts.js
    	new file:   lib/notifications.js
    	new file:   lib/permissions.js
    	new file:   lib/router.js
    	new file:   packages/errors/README.md
    	new file:   packages/errors/errors.js
    	new file:   packages/errors/errors_list.html
    	new file:   packages/errors/errors_list.js
    	new file:   packages/errors/errors_tests.js
    	new file:   packages/errors/package.js
    	modified:   server/fixtures.js
    	modified:   server/publications.js

---
+++ packages/errors/README.md
@@ -0,0 +1,5 @@
+I am learning the tutorial.
+
+This is the test-errors I generate based on the tutorial.
+
+Thanks for all authors!
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,16 @@
-Meteor.publish('posts', function() {
+// Meteor.publish('posts', function() {
+  // return Posts.find({flagged: false, author: author});
+// });
+
+Meteor.publish('posts', function(){
   return Posts.find();
+});
+
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
 });
